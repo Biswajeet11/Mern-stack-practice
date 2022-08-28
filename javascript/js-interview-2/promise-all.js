f0f922421885e7d1e10c2488a023b8f6d9081d35
Biswajeet  Gope <bisajeetgp25@gmail.com>
2022-08-28 20:10:19 +0530

    promise

--- javascript/js-interview-2/promise-all.js
+++ javascript/js-interview-2/promise-all.js
@@ -3,11 +3,33 @@
 function showText(text, time) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-             resolve(text);
+            resolve(text);
         }, time)
     })
 }
 
-Promise.all([showText("hello",1000), Promise.resolve("Hi")]).then((value)=>{
+Promise.all([showText("hello", 1000), Promise.resolve("Hi")]).then((value) => {
     console.log(value)
 })
+
+
+function myPromisesAll(promises) {
+    let result = [];
+    return new Promise((resolve, reject) => {
+        promises.forEach((promise, index) => {
+            promise.then((p) => {
+                result.push(p);
+                if(index == promise.length-1){
+                    resolve(result)
+                }
+            }).catch((err) => reject(err));
+        })
+    })
+}
+
+
+myPromisesAll([showText("hello", 1000), Promise.resolve("Hi"), Promise.reject("bye")]).then((value) => {
+    console.log(value)
+})
+
+
